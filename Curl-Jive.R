3d22d50992b13e6221b08ed0ec24a7b4b47cbae2
Tim <Tim.Reid@ca.com>
2015-06-09 12:31:41 -0400

    restructured and added likes, etc

--- Curl-Jive.R
+++ Curl-Jive.R
@@ -2,6 +2,12 @@ library(RCurl)
 library(jsonlite)
 library(XML)
 library(httr)
+library(data.table)
+
+f6 = function(x) {
+  ### count unique occurences within a numeric vector
+  data.table(x)[, .N, keyby = x]
+}
 
 options(stringsAsFactors = FALSE)
 
@@ -26,19 +32,18 @@ opts         <- curlOptions(header = FALSE, verbose=TRUE, netrc = TRUE)
 cmd1 <- "auth/login?" 
 auth <- postForm(paste(url1,cmd1,clientId,"&",clientSecret,sep=""))
 
-### Use date range of last 7 days
+### Use date range of last 2 days
 after  <- paste("after=",as.Date(Sys.Date()) - 2,"T00:00:00-0400&",sep="")
 
 ### look for recent popular unanswered questions
-filt1   <- paste("filter=match(resolved,false)&",           # unanswered
-                  "activity(view)&",                        # views
-                  "type(thread)&",                          # from a thread
-                  "match(isQuestion,true)&",                # its a question
-                  after,
-                  "count=all",sep="")                       # get all of them
+filt1   <- paste("filter=match(resolved,false)&",          # unanswered
+                 "activity(view)&",                        # views
+                 "type(thread)&",                          # from a thread
+                 "match(isQuestion,true)&",                # its a question
+                 after,
+                 "count=all",sep="")                       # get all of them
 
-url1  <- "https://api.jivesoftware.com/analytics/v2/export/activity/?"
-req   <- GET(paste(url1,filt1,sep=""), 
+req   <- GET(paste(url2,"export/activity/?",filt1,sep=""), 
              config(httpheader=c("Authorization"= auth[1]))) # get the url
 json  <- content(req, as = "text")                           # parse
 act   <- fromJSON(json)                                      # convert from JSON
@@ -79,65 +84,50 @@ activityType  <- act$list$activityType
 containerType <- act$list$containerType
 name          <- act$list$activity$destination$name
 
-df <- data.frame(activityName,timestamps,datetime,actorIDs,actionObjId,
-                 containerId,username,email,actorType,
-                 objectType,open,objectId,status,
-                 activityType,containerType,name)
+dfq <- data.frame(activityName,timestamps,datetime,actorIDs,actionObjId,
+                  containerId,username,email,actorType,
+                  objectType,open,objectId,status,
+                  activityType,containerType,name)
 
 ### Remove Expert Forums
-df <- df[substr(df$name,nchar(df$name)-2,nchar(df$name)) != " EF", ]
+dfq <- dfq[substr(dfq$name,nchar(dfq$name)-2,nchar(dfq$name)) != " EF", ]
 
-write.csv(df,"Unresolved Questions.csv")
-
-### now look at views
-filt2 <- paste("filter=name(activity_view_thread)&",         # get views
-                 after,
-                 "count=all",sep="")                         # get all of them
-
-req  <- GET(paste(url1,filt2,sep=""), 
-             config(httpheader=c("Authorization"= auth[1]))) # get the url
-json  <- content(req, as = "text")                           # parse
-act   <- fromJSON(json)                                      # convert from JSON
-
-activityName <- act$list$name
-actorIDs      <- act$list$actorID
-actionObjId   <- act$list$actionObjectId
-objectType    <- act$list$activity$actionObject$objectType
-objectId      <- act$list$activity$actionObject$objectId
-
-name          <- act$list$activity$destination$name
-
-df <- data.frame(activityName,actorIDs,actionObjId,
-                 objectType,objectId,name)
-
-### Remove Expert Forums
-df <- df[substr(df$name,nchar(df$name)-2,nchar(df$name)) != " EF", ]
-
-write.csv(df,"Views.csv")
-
-### Finally, look at Likes, Comments, etc. to measure popularity
-filt3 <- paste("filter=name(activity_like_thread)&",         # get views
-               "activity_like_comment",
-               "activity_update_question",
-               "activity_update_message)",
+### now look at views, likes, etc.
+filt2 <- paste("filter=name(activity_view_thread,",          # get views
+               "activity_like_thread,",                      # get likes
+               "activity_follow_thread,",                    # follows
+               "activity_aided_question,",
+               "activity_rate_thread,",
+               "activity_update_question)&",                 # get updates
                after,
-               "count=all",sep="")                         # get all of them
+               "count=all",sep="")                           # get all of them
 
-req  <- GET(paste(url1,filt3,sep=""), 
-            config(httpheader=c("Authorization"= auth[1]))) # get the url
+req  <- GET(paste(url2,"export/activity/?",filt2,sep=""), 
+            config(httpheader=c("Authorization"= auth[1])))  # get the url
 json  <- content(req, as = "text")                           # parse
 act   <- fromJSON(json)                                      # convert from JSON
 
-activityName <- act$list$name
-actionObjId   <- act$list$actionObjectId
-objectType    <- act$list$activity$actionObject$objectType
-objectId      <- act$list$activity$actionObject$objectId
-name          <- act$list$activity$destination$name
+v.activityName  <- act$list$name
+v.actorIDs      <- act$list$actorID
+v.timestamps    <- act$list$timestamp
+v.datetime      <- as.POSIXct(v.timestamps/1000, origin="1970-01-01")
+v.actionObjId   <- act$list$actionObjectId
+v.objectType    <- act$list$activity$actionObject$objectType
+v.objectId      <- act$list$activity$actionObject$objectId
+v.name          <- act$list$activity$destination$name
 
-df <- data.frame(activityName,actionObjId,
-                 objectType,objectId,name)
+dfv <- data.frame(v.activityName,v.actorIDs,v.datetime,v.actionObjId,
+                  v.objectType,v.objectId,v.name)
 
-# Remove Expert Forums
-df <- df[substr(df$name,nchar(df$name)-2,nchar(df$name)) != " EF", ]
+### Remove Expert Forums
+dfv <- dfv[substr(dfv$v.name,nchar(dfv$v.name)-2,nchar(dfv$v.name)) != " EF", ]
 
-write.csv(df,"Likes.csv")
+write.csv(dfv,"Views.csv")
+
+### Update the unanswered questions with the counts of views, likes, etc.
+dfq       <- dfq[order(dfq$actionObjId),]          
+views     <- f6(v.actionObjId)
+dfq$views <- (merge(dfq$actionObjId, views, by = 'x'))[,-1]
+
+write.csv(dfq,"Unresolved Questions.csv")
+
